441661517844694618535929ee708ae62be3d18b
Xab3r <mail.xaber@gmail.com>
2015-07-04 20:27:49 +0300

    Initial commit

---
+++ NullCheckTestApp/NullCheckCompetition.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Linq.Expressions;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Tasks;
+
+namespace NullCheckTestApp
+{
+    [Task(platform: BenchmarkPlatform.X86, jitVersion: BenchmarkJitVersion.CurrentJit)]
+    public class NullCheckCompetition
+    {
+        private object m_nullObjectToCheck = default(object);
+        private object m_nonNullObjectToCheck = new object();
+
+        [Benchmark()]
+        public void FailAnonymousClass()
+        {
+            NullCheckThroughAnonymousClass(m_nullObjectToCheck, new { m_nullObjectToCheck });
+        }
+
+        [Benchmark()]
+        public void FailLazyAnonymousClass()
+        {
+            NullCheckThroughLazyAnonymousClass(m_nullObjectToCheck, () => new { m_nullObjectToCheck });
+        }
+
+        [Benchmark()]
+        public void FailRawCheck()
+        {
+            NullCheckThroughRawCheck(m_nullObjectToCheck, "m_nullObjectToCheck");
+        }
+
+        [Benchmark()]
+        public void FailSingleLambda()
+        {
+            NullCheckThroughSingleLambda(m_nullObjectToCheck, () => m_nullObjectToCheck);
+        }
+
+        [Benchmark()]
+        public void FailDoubleLambda()
+        {
+            NullCheckThroughDoubleLambda(m_nullObjectToCheck, () => () => m_nullObjectToCheck);
+        }
+
+
+        [Benchmark()]
+        public void SuccessAnonymousClass()
+        {
+            NullCheckThroughAnonymousClass(m_nonNullObjectToCheck, new { m_objectToCheck = m_nonNullObjectToCheck });
+        }
+
+        [Benchmark()]
+        public void SuccessLazyAnonymousClass()
+        {
+            NullCheckThroughLazyAnonymousClass(m_nonNullObjectToCheck, () => new { m_objectToCheck = m_nonNullObjectToCheck });
+        }
+
+        [Benchmark()]
+        public void SuccessRawCheck()
+        {
+            NullCheckThroughRawCheck(m_nonNullObjectToCheck, "m_nonNullObjectToCheck");
+        }
+
+        [Benchmark()]
+        public void SuccessSingleLambda()
+        {
+            NullCheckThroughSingleLambda(m_nonNullObjectToCheck, () => m_nonNullObjectToCheck);
+        }
+
+        [Benchmark()]
+        public void SuccessDoubleLambda()
+        {
+            NullCheckThroughDoubleLambda(m_nonNullObjectToCheck, () => () => m_nonNullObjectToCheck);
+        }
+
+        public static string NullCheckThroughLazyAnonymousClass<T>(object _objectToValidate, Func<T> _containerProvider) where T : class
+        {
+            if (_objectToValidate != null)
+            {
+                return string.Empty;
+            }
+
+            return NullCheckThroughAnonymousClass(_objectToValidate, _containerProvider());
+        }
+
+        public static string NullCheckThroughAnonymousClass<T>(object _objectToValidate, T _container) where T : class
+        {
+            if (_objectToValidate != null || _container == null)
+            {
+                return string.Empty;
+            }
+
+            var properties = typeof(T).GetProperties();
+            if (properties.Length != 1)
+            {
+                throw new ArgumentException(string.Format("Expected anonymous class with exactly one property, got {0}", typeof(T)));
+            }
+            return properties[0].Name;
+        }
+
+
+        public static string NullCheckThroughRawCheck(object _objectToValidate, string _objectName)
+        {
+            if (_objectToValidate != null || _objectName == null)
+            {
+                return string.Empty;
+            }
+
+            return _objectName;
+        }
+
+        public static string NullCheckThroughSingleLambda(object _objectToValidate, Expression<Func<object>> _expression)
+        {
+            if (_objectToValidate != null || _expression == null)
+            {
+                return string.Empty;
+            }
+            return GetName(_expression);
+        }
+
+        public static string NullCheckThroughDoubleLambda(object _objectToValidate, Func<Expression<Func<object>>> _expression)
+        {
+            if (_objectToValidate != null || _expression == null)
+            {
+                return string.Empty;
+            }
+            return GetName(_expression());
+        }
+
+        private static string GetName(Expression<Func<object>> exp)
+        {
+            MemberExpression body = exp.Body as MemberExpression;
+
+            if (body == null)
+            {
+                UnaryExpression ubody = (UnaryExpression)exp.Body;
+                body = ubody.Operand as MemberExpression;
+            }
+
+            return body.Member.Name;
+        }
+    }
+}
---
+++ NullCheckTestApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Tasks;
+
+namespace NullCheckTestApp
+{
+    public class Program
+    {
+        public static void Main(string[] _args)
+        {
+            try
+            {
+                Console.WindowWidth = 120;
+                Console.WindowHeight = 60;
+            }
+            catch (Exception)
+            {
+                //
+            }
+
+            new BenchmarkRunner().RunCompetition(new NullCheckCompetition(), new BenchmarkSettings(1, 1));
+        }
+
+    }
+}
